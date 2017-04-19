28cf7db0e275d87724f071c867ba0f6fb1ab431b
Pierre JANKOWIEZ <pjankowiez@BTIB.priv>
2017-04-19 12:15:33 +0200

    AC-92 Modify gitignore and build exe

--- build.bat
+++ build.bat
@@ -1,13 +1,51 @@
 @echo off
-set /p Input=Dossier du module a compiler ? (ex: themeBtib-ux) :
 
+:: BatchGotAdmin
+:-------------------------------------
+REM  --> Check for permissions
+    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
+>nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
+) ELSE (
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+)
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"=""
+    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+    pushd "%CD%"
+    CD /D "%~dp0"
+:--------------------------------------
+:: set the environment variable
+IF defined NIAGARA_HOME goto CONTINUE
+
+set /p niagara_home=Dossier d'installation de Niagara ? (ex: C:\Niagara\Niagara-4.3.58.4) :
+setx NIAGARA_HOME "%niagara_home%" /m || goto END
+
+
+:CONTINUE
 :: change directory or exit
 pushd "%Input%" || goto :END
+
+:: install global dependencies
+call npm install -g phantomjs grunt-cli
 
 :: install dev dependencies
 call npm install
 :: compile less files
-call node -e "require('grunt').tasks('less');"
+call grunt less
 popd
 :: build script
 if "%1" == "--skip" goto :DONE
