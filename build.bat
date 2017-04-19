73be39f76af40238ecf8c59f4e90d539daae6c54
Pierre JANKOWIEZ <pjankowiez@BTIB.priv>
2017-04-19 14:54:18 +0200

    AC-92 Create executable files for workflow

--- build.bat
+++ build.bat
@@ -1,55 +1,29 @@
 @echo off
 
-:: BatchGotAdmin
-:-------------------------------------
-REM  --> Check for permissions
-    IF "%PROCESSOR_ARCHITECTURE%" EQU "amd64" (
->nul 2>&1 "%SYSTEMROOT%\SysWOW64\cacls.exe" "%SYSTEMROOT%\SysWOW64\config\system"
-) ELSE (
->nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
-)
+call node %~dp0.lib/index.js create
 
-REM --> If error flag set, we do not have admin.
-if '%errorlevel%' NEQ '0' (
-    echo Requesting administrative privileges...
-    goto UACPrompt
-) else ( goto gotAdmin )
-
-:UACPrompt
-    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
-    set params = %*:"=""
-    echo UAC.ShellExecute "cmd.exe", "/c ""%~s0"" %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
-
-    "%temp%\getadmin.vbs"
-    del "%temp%\getadmin.vbs"
-    exit /B
-
-:gotAdmin
-    pushd "%CD%"
-    CD /D "%~dp0"
-:--------------------------------------
-:: set the environment variable
-IF defined NIAGARA_HOME goto CONTINUE
-
-set /p niagara_home=Dossier d'installation de Niagara ? (ex: C:\Niagara\Niagara-4.3.58.4) :
-setx NIAGARA_HOME "%niagara_home%" /m || goto END
-
-
-:CONTINUE
 :: change directory or exit
-pushd "%Input%" || goto :END
+pushd .tmp || goto :END
 
-:: install global dependencies
-call npm install -g phantomjs grunt-cli
+:: get folder name using node script
+for /f "delims=" %%i in ('node %~dp0.lib/index.js get-folder') do set input=%%i
 
-:: install dev dependencies
+pushd %input% || goto :END
+
 call npm install
 :: compile less files
 call grunt less
+
 popd
+
 :: build script
 if "%1" == "--skip" goto :DONE
 call %~dp0.lib/gradlew.bat build
+call %~dp0.lib/gradlew.bat --stop
+
+popd
+
+call node %~dp0.lib/index.js delete
 
 :END
 pause
